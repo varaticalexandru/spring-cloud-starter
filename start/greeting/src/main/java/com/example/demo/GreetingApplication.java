202c50d643731770936c5233f832bb739697ab80
alex <varaticalexandru1@gmail.com>
2024-04-04 11:54:32 +0300

    project init

---
+++ start/greeting/src/main/java/com/example/demo/GreetingApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigurationProperties(GreetingProperties.class)
+public class GreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingApplication.class, args);
+	}
+
+	@Bean
+	public Sampler sampler() {
+		return new AlwaysSampler();
+	}
+
+}
---
+++ start/web/src/main/java/com/example/demo/WebApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+	@Configuration
+	static class MyConfig {
+		@Bean
+		public RestTemplate rest() {
+			return new RestTemplateBuilder().build();
+		}
+	}
+}
